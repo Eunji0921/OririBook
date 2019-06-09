1aa994200324cccec122fa21676e40d2109fe136
Ori <eunji344@naver.com>
2019-06-10 00:50:00 +0900

    Publish Docs

---
+++ publish_vuepress.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+# set -e
+
+npm install && vuepress build
+
+git checkout master
+
+git pull origin master --rebase
+
+cp -R .vuepress/dist/* .
+
+git add .
+
+git commit -a -m 'Publish Docs'
+
+git push origin master
+
+git checkout draft
